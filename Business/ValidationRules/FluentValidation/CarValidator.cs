c50eaa15caf46c7440936659d331ac366bce7be2
yiğit solak <ygtsolak@hotmail.com>
2021-02-22 17:33:20 +0300

    api çalışması sorunlu tekrar bakılacak

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,28 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(p => p.CarName).NotEmpty();
+            RuleFor(p => p.CarName).MinimumLength(2);
+            RuleFor(p => p.DailyPrice).NotEmpty();
+            RuleFor(p => p.DailyPrice).GreaterThan(0);
+            RuleFor(p => p.DailyPrice).GreaterThanOrEqualTo(10).When(p => p.BrandId == 1);
+            RuleFor(p => p.CarName).Must(StartWithA);
+
+            bool StartWithA(string arg)
+            {
+                return arg.StartsWith("A");
+            }
+        }
+    }
+}
