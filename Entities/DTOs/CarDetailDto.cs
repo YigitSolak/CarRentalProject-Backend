f2b69c43415ab22fc35e5eb497fab02a8d66c7f2
DrCocktail <yigit.solak02@gmail.com>
2021-03-29 14:37:05 +0300

    hata çalışmları

--- Entities/DTOs/CarDetailDto.cs
+++ Entities/DTOs/CarDetailDto.cs
@@ -12,6 +12,6 @@ namespace Entities.DTOs
         public string ColorName { get; set; }
         public string Description { get; set; }
         public int ModelYear { get; set; }
-        public double DailyPrice { get; set; }
+        public decimal DailyPrice { get; set; }
     }
 }
